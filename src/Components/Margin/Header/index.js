d943e0a71bb2b106195c52e5713d7877fe69ee35
ncclymer <77338319+ncclymer@users.noreply.github.com>
2021-07-28 14:50:10 -0600

    added header component. Importing all routes. Exported to app.

---
+++ src/Components/Margin/Header/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Nav from "../Nav";
+import About from "../../Pages/About";
+import Portfolio from "../../Pages/Portfolio";
+import Contact from "../../Pages/Contact";
+import Resume from "../../Pages/Resume";
+import Footer from "../../Margin/Footer";
+
+const Header = () => {
+  const [currentPage, handlePageChange] = useState("About/");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Contact":
+        return <Contact />;
+      case "Resume":
+        return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+  return (
+    <div className="main-container">
+      <div>
+        <header>
+          <h2>Nick Clymer</h2>
+          <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        </header>
+      </div>
+      <main>
+        <div>{renderPage(currentPage)}</div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Header;
