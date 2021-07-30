57571d3649dd96260f4488a44ee8f8f9c5799b69
ncclymer <77338319+ncclymer@users.noreply.github.com>
2021-07-30 17:40:30 -0600

    additional styling, update to about paragraph

--- src/Components/Pages/Resume/index.js
+++ src/Components/Pages/Resume/index.js
@@ -36,7 +36,7 @@ const Home = () => {
             <ListGroup variant="flush">
               <ListGroup.Item>OOP</ListGroup.Item>
               <ListGroup.Item>API's</ListGroup.Item>
-              <ListGroup.Item>SPA's</ListGroup.Item>
+              <ListGroup.Item>MVC</ListGroup.Item>
               <ListGroup.Item>PWA's</ListGroup.Item>
             </ListGroup>
           </Card>
