f206d49f8d86b9ac47088ce72e48771a54254286
ncclymer <77338319+ncclymer@users.noreply.github.com>
2021-07-30 19:13:25 -0600

    gh pages deployment fix

--- src/Components/Pages/About/index.js
+++ src/Components/Pages/About/index.js
@@ -11,7 +11,7 @@ const About = () => {
           I am a full stack developer that is passionate about honing my current skills and seeking new technologies. I am a Graduate of University of Denver’s 
           Coding Bootcamp. Proficient with front-end technologies HTML, CSS, JavaScript and React, and skilled in back-end technologies such as Node, SQL, Sequelize,
           MongoDB, and Graphql. During my time at University of Denvers’s Coding Bootcamp I have had the opportunity to work on over 20 projects ranging from simple 
-          front-end web design to full stack applications. I am a strong independent worker and gladlyt welcome team collaboration. My goal is to provide an optimal
+          front-end web design to full stack applications. I am a strong independent worker and gladly welcome team collaboration. My goal is to provide an optimal
           user experience that increases appliction profitability.
         </Card.Body>
       </Card>
